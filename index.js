4adce127bf610b63b35487593824d7a10f0a1fa0
bryce-sncf <bryce.marcol@sncf.fr>
2023-04-17 10:42:10 +0200

    toto

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+import chalk from 'chalk';
+
+
+console.log(chalk.red("Jamal"))
+console.log(chalk.blue("Matt"))
+console.log(chalk.yellow("Yannick"))
+console.log(chalk.green("Rachid"))
+
+
+// const { default: chalk } = require('chalk');
+// const userInfo = require('./userInformation');
+// console.log(`Hi, my name is ${userInfo.name} and I'm ${userInfo.age} years old`);
+// userInfo.displayPassions();
